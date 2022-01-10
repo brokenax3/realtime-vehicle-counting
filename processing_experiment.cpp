bef146c67ac46938e8b42bb844a0b9fd95fee2b7
Mark Lee <mlcy0508@gmail.com>
2022-01-10 11:29:55 +1100

    Current Stable Parameters
    Gaussian Blur with Kernel Size(3, 3)
    Image thresholding with a range of (30, 255)
    Erosion with Size(2, 2)
    Dilation with Size(5, 5)

--- processing_experiment.cpp
+++ processing_experiment.cpp
@@ -7,38 +7,13 @@
 using namespace cv;
 using namespace std;
 
-Mat makeSkewImg(Mat imgInput)
-{
-    float w = 200, h = 400;
-    Mat imgWarp;
-
-    Point2f src[4] = {
-        {144,0},
-        {220,0},
-        {9,200},
-        {362,200}
-    };
-
-    Point2f dst[4] = {
-        {0.0f, 0.0f},
-        {w, 0.0f},
-        {0.0f, h},
-        {w, h}
-    };
-    
-    Mat matrix = getPerspectiveTransform(src, dst);
-    warpPerspective(imgInput, imgWarp, matrix, Point(w, h));
-    /* imshow("Warp", imgWarp); */
-    return imgWarp;
-}
-
 void getContours(Mat imgInput, Mat img)
 {
 
     vector<vector<Point>> contours;
 
     findContours(imgInput, contours, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
-    /* vector<Rect> boundRect(contours.size()); */
+    vector<Rect> boundRect(contours.size());
     vector<vector<Point>> hull(contours.size());
     Mat drawing = Mat::zeros(imgInput.size(), CV_8UC3);
     for( size_t i = 0; i < contours.size(); i++ )
@@ -49,39 +24,36 @@ void getContours(Mat imgInput, Mat img)
     for(int i = 0; i < contours.size(); i++)
     {
         Scalar color = Scalar(255, 0, 0);
-        /* fillPoly(drawing, pts=[contours], color); */
-        /* drawContours(drawing, contours, i, color); */
         drawContours(drawing, hull, i, color);
     }
 
     imshow("Drawing", drawing);
 
+    int count = 0;
+    for(vector<Point> contour : contours)
+    {
+        int area = contourArea(contour);
+        /* cout << area << endl; */
+        if((2000 < area) && (area < 5000))
+        {
+            cout << area << endl;
+            /* drawContours(img, conPoly, count, Scalar(0, 255, 0), 2); */
 
-    /* for(vector<Point> contour : contours) */
-    /* { */
-    /*     int area = contourArea(contour); */
-    /*     /1* cout << area << endl; *1/ */
-    /*     if((1500 < area) && (area < 5000)) */
-    /*     { */
-    /*         cout << area << endl; */
-    /*         /1* drawContours(img, conPoly, count, Scalar(0, 255, 0), 2); *1/ */
+            boundRect[count] = boundingRect(contour);
 
-    /*         boundRect[count] = boundingRect(contour); */
-    /*         if(boundRect[count].area() >= 2000) */
-
-    /*         rectangle(img, boundRect[count].tl(), boundRect[count].br(), Scalar(255, 0, 0), 5); */
-    /*         cout << boundRect[count].tl() << endl; */
-    /*         cout << boundRect[count].br() << endl; */
-    /*     } */
-    /*     count++; */
-    /* } */
+            rectangle(img, boundRect[count].tl(), boundRect[count].br(), Scalar(255, 0, 0), 5);
+            /* cout << boundRect[count].tl() << endl; */
+            /* cout << boundRect[count].br() << endl; */
+        }
+        count++;
+    }
 }
 
 int main()
 {
     /* VideoCapture cap("test_video/hd_autobitrate_close_view.mp4"); */
     VideoCapture cap("test_video/low_angle.mp4");
-    Mat img, imgNext, imgDiff;
+    Mat img, imgNext, imgDiff, imgOriginal;
 
     Mat imgGrey, imgBlur, imgCanny, imgDilate, imgBlurBlur, mask, imgErode;
 
@@ -92,28 +64,30 @@ int main()
     while(true)
     {
         img = imgNext;
-        cap.read(imgNext);
+        cap.read(imgOriginal);
+        resize(imgOriginal, imgOriginal, Size(), 0.5, 0.5, INTER_LINEAR);
+        imgNext = imgOriginal;
         cvtColor(imgNext, imgNext, COLOR_BGR2GRAY);
-        resize(imgNext, imgNext, Size(), 0.5, 0.5, INTER_LINEAR);
         if(firstFrame == 0)
         {
             absdiff(imgNext, img, imgDiff);
             /* medianBlur(imgDiff, imgDiff, 3); */
-            GaussianBlur(imgDiff, imgDiff, Size(1, 1), 0);
+            GaussianBlur(imgDiff, imgDiff, Size(3, 3), 0);
             threshold(imgDiff, mask, 30, 255, THRESH_BINARY);
-            Mat kernel = getStructuringElement(MORPH_RECT, Size(4, 4));
+            Mat kernel = getStructuringElement(MORPH_RECT, Size(5, 5));
             Mat ekernel = getStructuringElement(MORPH_RECT, Size(2, 2));
             erode(mask, imgErode, ekernel);
-            dilate(mask, imgDilate, kernel);
+            dilate(imgErode, imgDilate, kernel);
             /* morphologyEx(mask, imgErode, MORPH_CLOSE, kernel); */
             /* morphologyEx(imgErode, imgErode, MORPH_CLOSE, kernel); */
             /* morphologyEx(imgErode, imgErode, MORPH_OPEN, ekernel); */
             /* erode(mask, imgErode, ekernel); */
-            getContours(imgDilate, mask);
+            getContours(imgDilate, imgOriginal);
             imshow("Difference", imgDiff);
             imshow("Threshold", mask);
             imshow("Dilated", imgDilate);
             imshow("Eroded", imgErode);
+            imshow("Detection Image", imgOriginal);
             waitKey(30);
         }
         else
@@ -121,42 +95,4 @@ int main()
             firstFrame = 0;
         }
     }
-        
-
-    /* while(true) */
-    /* { */
-    /*     cap.read(img); */
-
-    /*     if(img.empty()) */
-    /*     { */
-    /*         cap.set(CAP_PROP_POS_AVI_RATIO, 0); */
-    /*         cap.read(img); */
-    /*     } */
-    /*     resize(img, img, Size(), 0.2, 0.2, INTER_LINEAR); */
-    /*     cvtColor(img, imgGrey, COLOR_BGR2GRAY); */
-    /*     GaussianBlur(imgGrey, imgBlur, Size(5, 5), 0); */
-    /*     pBacksub -> apply(imgBlur, mask); */
-
-
-    /*     Mat ekernel = getStructuringElement(MORPH_RECT, Size(5, 5)); */
-    /*     /1* erode(mask, imgErode, ekernel); *1/ */
-    /*     /1* dilate(imgErode, imgDilate, kernel); *1/ */
-    /*     /1* dilate(imgDilate, imgDilate, kernel); *1/ */
-    /*     /1* dilate(imgDilate, imgDilate, kernel); *1/ */
-    /*     morphologyEx(mask, imgTemp, MORPH_OPEN, ekernel); */
-    /*     morphologyEx(imgTemp, imgErode, MORPH_CLOSE, kernel); */
-    /*     morphologyEx(imgErode, imgDilate, MORPH_OPEN, kernel); */
-
-    /*     getContours(imgDilate, img); */
-
-    /*     /1* imshow("Test Video", img); *1/ */
-    /*     /1* imshow("Test Video Blur", imgBlur); *1/ */
-    /*     /1* /2* imshow("Test Video Cammy", imgCanny); *2/ *1/ */
-    /*     /1* imshow("Test video", imgDilate); *1/ */
-    /*     imshow("Image", img); */
-    /*     /1* imshow("Mask", mask); *1/ */
-    /*     imshow("Mask Erode", imgErode); */
-    /*     imshow("Mask Dilate", imgDilate); */
-    /*     waitKey(30); */
-    /* } */
 }
