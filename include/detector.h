4b90091f2b9371500b87482820a18921d78b4d8b
Mark Lee <mlcy0508@gmail.com>
2022-02-14 00:50:11 +1100

    Restructure and modified to use YOLOV5

---
+++ include/detector.h
@@ -0,0 +1,79 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <opencv2/opencv.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/dnn.hpp>
+
+using namespace std;
+using namespace cv;
+
+struct PadInfo
+{
+    float scale;
+    int top;
+    int left;
+};
+
+struct Detection
+{
+    PadInfo info;
+    std::vector<Mat> detection;
+    float inference;
+};
+
+class Colors
+{
+    public:
+        vector<string> hex_str;
+        vector<Scalar> palette;
+        int n = 20;
+        Colors():hex_str(20,"")
+        {
+            this->hex_str = {
+                "FF3838", "FF9D97", "FF701F", "FFB21D", "CFD231", "48F90A", "92CC17", "3DDB86", "1A9334", "00D4BB",
+                "2C99A8", "00C2FF", "344593", "6473FF", "0018EC", "8438FF", "520085", "CB38FF", "FF95C8", "FF37C7"};
+            for (auto &ele : this->hex_str)
+            {
+                palette.push_back(hex2rgb(ele));
+            }
+        }
+        Scalar hex2rgb(string &hex_color)
+        {
+            int b, g, r;
+            sscanf(hex_color.substr(0, 2).c_str(), "%x", &r);
+            sscanf(hex_color.substr(2, 2).c_str(), "%x", &g);
+            sscanf(hex_color.substr(4, 2).c_str(), "%x", &b);
+            return Scalar(b, g, r);
+        }
+};
+
+struct Config
+{
+    float confThreshold;
+    float nmsThreshold;
+    string weightPath;
+    string classNamePath;
+    Size size;
+    bool _auto;
+};
+
+class Detector
+{
+    public:
+        Detector(Config &config);
+        Detection detect(Mat &img);
+        void postProcess(Mat &img, Detection &detection, Colors&cl);
+        PadInfo letterbox(Mat &img, Size new_shape, Scalar color, bool _auto, bool scaleFill, bool scaleup, int stride);
+
+    private:
+        float nmsThreshold;
+        float confThreshold;
+        Size inSize;
+        bool _auto;
+        vector<string> classNames;
+        cv::dnn::Net model;
+        void drawPrediction(Mat &img, vector<Rect> &boxes, vector<float> &sc, vector<int> &clsIndexs, vector<int> &ind, Colors&cl);
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,50 +1,28 @@
 #include <iostream>
 #include <boost/format.hpp>
 #include <stdlib.h>
-#include <opencv2/tracking.hpp>
-/* #include <opencv2/tracking/tracking_legacy.hpp> */
 
-#ifndef __CONSTANT_H__
-#include "constants.h"
-#endif
+// #ifndef __CONSTANT_H__
+// #include "constants.h"
+// #endif
 
-#include "dnn_processing.h"
-#include "tracking.h"
+#include "detector.h"
 
-
-std::vector<std::string> classes;
+using namespace std;
+using namespace cv;
 
 int main()
 {
-    // Initial Variables
-    int initialConf = (int)(confThreshold * 100);
-    std::string modelPath = "./dnn_files/yolov4-tiny.weights";
-    std::string configPath = "./dnn_files/yolov4-tiny.cfg";
+    string model_path = "./dnn_files/yolov5s.onnx";
+    Config config = {0.25f, 0.45f, model_path, "./dnn_files/coco.names", cv::Size(640, 640), false};
+    Detector detector(config);
 
-    // Unpack Class Names
-    std::string classPath = "./dnn_files/coco_classes.txt";
-    std::ifstream ifs(classPath.c_str());
-    std::string line;
-
-    while(std::getline(ifs, line))
-    {
-        classes.push_back(line);
-    }
-     
     // Load a model.
-    cv::dnn::Net net = cv::dnn::readNet(modelPath, configPath);
-
-    // CUDA for Processing
-    net.setPreferableBackend(backend);
-    net.setPreferableTarget(target);
-    std::vector<cv::String> outNames = net.getUnconnectedOutLayersNames();
+    cv::dnn::Net net = cv::dnn::readNetFromONNX(model_path);
 
     // Create a window
-    static const std::string kWinName = "Deep learning object detection in OpenCV";
-    static const std::string test = "test";
-    /* static const std::string detectionWinName = "Detection Window"; */
-    namedWindow(kWinName, cv::WINDOW_NORMAL);
-    /* namedWindow(detectionWinName, cv::WINDOW_NORMAL); */
+    static const string kWinName = "Deep learning object detection in OpenCV";
+    namedWindow(kWinName, WINDOW_NORMAL);
 
     // Open a video file or an image file or a camera stream.
     cv::VideoCapture cap;
@@ -53,54 +31,16 @@ int main()
 
     // Process frames.
     cv::Mat frame;
-    cv::Mat drawing;
     
-    CentroidTracker centroidTracker; 
-    // Output of Neural Network
-    std::vector<cv::Rect> rectangles;
-    int count = 0;
     while (true)
     {
         cap >> frame;
-        drawing = cv::Mat(frame.cols, frame.rows, CV_8UC3, cv::Scalar());
-        /* cv::GaussianBlur(frame, frame, cv::Size(3, 3), 0); */
-        /* roi = frame(cv::Range(150, 300), cv::Range(600, 1016)); */
-        
-        /* if (frame.empty()) */
-        /* { */
-        /*     cv::waitKey(); */
-        /*     break; */
-        /* } */
-        /* std::cout << count << std::endl; */
-        // Detection
-        preprocess(frame, net, cv::Size(inpWidth, inpHeight), scale);
+        Detection detection = detector.detect(frame);
+        Colors cl = Colors();
 
-        std::vector<cv::Mat> outs;
-        net.forward(outs, outNames);
-
-        rectangles = postprocess(frame, outs, net, backend);
-
-        centroidTracker.updateObjects(rectangles);
-        
-        for(cv::Rect rectangle : rectangles)
-        {
-            cv::rectangle(drawing, rectangle, cv::Scalar(255, 0, 0), 2);
-        }
-
-        for(Object object : centroidTracker.objects)
-        {
-            putText(drawing, std::to_string(object.id), cv::Point(object.centroid.x, object.centroid.y), cv::FONT_HERSHEY_PLAIN, 1.5, cv::Scalar(0, 255, 0), 2);
-        }
-
-        // Put efficiency information.
-        std::vector<double> layersTimes;
-        double freq = cv::getTickFrequency() / 1000;
-        double t = net.getPerfProfile(layersTimes) / freq;
-        std::string label = cv::format("Inference time: %.2f ms", t);
-        cv::putText(frame, label, cv::Point(0, 15), cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(0, 255, 0));
+        detector.postProcess(frame, detection, cl);
 
         imshow(kWinName, frame);
-        imshow(test, drawing);
         cv::waitKey(30);
     }
 }
